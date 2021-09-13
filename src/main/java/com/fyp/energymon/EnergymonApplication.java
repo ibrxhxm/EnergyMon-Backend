2ad8d0eb0ca2a584281bdbf34a01ed765d59e3b5
Ibrahim Ayub <ibrahimayubadinoyi@gmail.com>
2021-09-13 06:05:51 +0100

    initial commit

---
+++ src/main/java/com/fyp/energymon/EnergymonApplication.java
@@ -0,0 +1,13 @@
+package com.fyp.energymon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergymonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergymonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fyp/energymon/EnergymonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fyp.energymon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergymonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
