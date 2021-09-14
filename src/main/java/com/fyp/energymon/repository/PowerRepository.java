fb135df5b2ec56c792228112b1510a9c07c21d77
Ibrahim Ayub <ibrahimayubadinoyi@gmail.com>
2021-09-14 10:56:21 +0100

    remove redundant imports

--- src/main/java/com/fyp/energymon/repository/PowerRepository.java
+++ src/main/java/com/fyp/energymon/repository/PowerRepository.java
@@ -5,7 +5,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Timestamp;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Repository
